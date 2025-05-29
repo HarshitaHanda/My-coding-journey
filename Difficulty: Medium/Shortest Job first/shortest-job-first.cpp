c3322f09f7396c03566eec24152d5b650a5c3551
HarshitaHanda <149144740+HarshitaHanda@users.noreply.github.com>
2025-05-30 01:48:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    long long solve(vector<int>& bt) {
+        // code here
+        sort(bt.begin(),bt.end());
+        int wt=0,t=0;
+        for(int i=0;i<bt.size();i++){
+            wt+=t;
+            t+=bt[i];
+        } return (wt/bt.size());
+    }
+};
