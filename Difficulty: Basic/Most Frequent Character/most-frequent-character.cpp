000f6066d83d65cdbc12769d0b8e21669a62d8bb
HarshitaHanda <149144740+HarshitaHanda@users.noreply.github.com>
2025-06-17 08:33:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Most Frequent Character/most-frequent-character.cpp
+++ Difficulty: Basic/Most Frequent Character/most-frequent-character.cpp
@@ -5,15 +5,19 @@ class Solution {
     // Function to find the maximum occurring character in a string.
     char getMaxOccuringChar(string& s) {
         // Your code here
-        map<char, int> mp;
-        for(char i: s){
-            mp[i]++;
-        } int cnt=INT_MIN; char ch;
-        for(auto it: mp){
-            if(it.second>cnt){
-                cnt=it.second;
-                ch=it.first;
+        int arr[26]={0};
+        for(int i=0;i<s.length();i++){
+            int x=s[i]-'a';
+            arr[x]++;
+        }
+        int maxi=-1, ans=0;
+        for(int i=0;i<26;i++){
+            if(maxi<arr[i]){
+                maxi=arr[i];
+                ans=i;
             }
-        } return ch;
+        }
+        ans=ans+'a';
+        return ans;
     }
 };
